2552079c2db8fe3c9846fc981e7c590846fbf95c
Rizal Miftahussalim <rmiftahussalim@gmail.com>
2022-02-17 15:04:06 +0700

    menghapus relasi many to many (account_tranfer, account_withdraw & account_save) menjadi one to many, menambah balance pada tabel account

--- project.py
+++ project.py
@@ -16,6 +16,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(15), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+    accounts = db.relationship('account', backref='user', lazy='dynamic')
     
     def __repr__(self):
         return f'Nama <{self.name}>'
@@ -25,6 +26,7 @@ class Branch(db.Model):
     branch_name = db.Column(db.String(120), unique=True, nullable=False)
     address = db.Column(db.String(200), unique=True, nullable=False)
     city = db.Column(db.String(50), nullable=False)
+    accounts = db.relationship('account', backref='branch', lazy='dynamic')
     
     def __repr__(self):
         return f'Cabang <{self.branch_name}>'
@@ -34,6 +36,10 @@ class Account(db.Model):
     number = db.Column(db.Integer, unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     branch_id = db.Column(db.Integer, db.ForeignKey('branch.id'), nullable=False)
+    balance = db.Column(db.Integer, nullable=False)
+    transfers = db.relationship('transfer', backref='account', lazy='dynamic')
+    withdraws = db.relationship('withdraw', backref='account', lazy='dynamic')
+    saves = db.relationship('save', backref='account', lazy='dynamic')
 
     def __repr__(self):
         return f'Rekening <{self.number}>'
@@ -41,9 +47,10 @@ class Account(db.Model):
 class Transfer(db.Model):
     id = db.Column(db.Integer, primary_key=True, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    nominal = db.Column(db.Integer, nullable=False)
     date = db.Column(db.Date, nullable=False)
-    accounts = db.relationship('Account', backref='transfer', lazy='dynamic')
+    from_account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+    to_account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+    nominal = db.Column(db.Integer, nullable=False)
     
     def __repr__(self):
         return f'Transfer sejumlah <{self.nominal}>'
@@ -51,9 +58,9 @@ class Transfer(db.Model):
 class Withdraw(db.Model):
     id = db.Column(db.Integer, primary_key=True, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
     nominal = db.Column(db.Integer, nullable=False)
     date = db.Column(db.Date, nullable=False)
-    accounts = db.relationship('Account', backref='withdraw', lazy='dynamic')
     
     def __repr__(self):
         return f'Tarik tunai sejumlah <{self.nominal}>'
@@ -61,28 +68,28 @@ class Withdraw(db.Model):
 class Save(db.Model):
     id = db.Column(db.Integer, primary_key=True, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
     nominal = db.Column(db.Integer, nullable=False)
     date = db.Column(db.Date, nullable=False)
-    accounts = db.relationship('Account', backref='save', lazy='dynamic')
     
     def __repr__(self):
         return f'Setor tunai sejumlah <{self.nominal}>'
     
-account_transfer = db.Table('account_transfer', db.Model.metadata,
-                            db.Column('transfer_id', db.ForeignKey('transfer.id'), primary_key=True),
-                            db.Column('from_account_id', db.ForeignKey('account.id'), primary_key=True),
-                            db.Column('to_account_id', db.ForeignKey('account.id'), primary_key=True)
-                            )
+# account_transfer = db.Table('account_transfer', db.Model.metadata,
+#                             db.Column('transfer_id', db.ForeignKey('transfer.id'), primary_key=True),
+#                             db.Column('from_account_id', db.ForeignKey('account.id'), primary_key=True),
+#                             db.Column('to_account_id', db.ForeignKey('account.id'), primary_key=True)
+#                             )
 
-account_withdraw = db.Table('account_withdraw', db.Model.metadata,
-                            db.Column('withdraw_id', db.ForeignKey('withdraw.id'), primary_key=True),
-                            db.Column('account_id', db.ForeignKey('account.id'), primary_key=True)
-                            )
+# account_withdraw = db.Table('account_withdraw', db.Model.metadata,
+#                             db.Column('withdraw_id', db.ForeignKey('withdraw.id'), primary_key=True),
+#                             db.Column('account_id', db.ForeignKey('account.id'), primary_key=True)
+#                             )
 
-account_save = db.Table('account_save', db.Model.metadata,
-                            db.Column('save_id', db.ForeignKey('save.id'), primary_key=True),
-                            db.Column('account_id', db.ForeignKey('account.id'), primary_key=True)
-                            )
+# account_save = db.Table('account_save', db.Model.metadata,
+#                             db.Column('save_id', db.ForeignKey('save.id'), primary_key=True),
+#                             db.Column('account_id', db.ForeignKey('account.id'), primary_key=True)
+#                             )
 
 db.create_all()
 db.session.commit()
